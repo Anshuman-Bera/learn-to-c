b86c7ada18687d8bfe62863d789ddd3330f2acd5
Anshuman-Bera <anshubera2005@gmail.com>
2024-03-17 01:07:47 +0530

    2nd c program

---
+++ CodeChef/PCL01_problems_INSTNOODLE.c
@@ -0,0 +1,39 @@
+// Chef and Instant Noodles
+// Chef has invented 
+// 1
+// 1-minute Instant Noodles. As the name suggests, each packet takes exactly 
+// 1
+// 1 minute to cook.
+
+// Chef's restaurant has 
+// �
+// X stoves and only 
+// 1
+// 1 packet can be cooked in a single stove at any minute.
+
+// How many customers can Chef serve in 
+// �
+// Y minutes if each customer orders exactly 
+// 1
+// 1 packet of noodles?
+
+// Input Format
+// The first and only line of input contains two space-separated integers 
+// �
+// X and 
+// �
+// Y — the number of stoves and the number of minutes, respectively.
+// Output Format
+// Print a single integer, the maximum number of customers Chef can serve in 
+// �
+// Y minutes
+
+#include <stdio.h>
+
+int main(void) {
+	int x,y,m;
+	scanf("%d%d",&x,&y);
+	m = x*y;
+	printf("%d",m);
+
+}
