9b805e7274c88a6606d3219c3484e13d6ba6e052
Anshuman-Bera <anshubera2005@gmail.com>
2024-03-17 11:50:01 +0530

    4th c program

---
+++ CodeChef/Course/LBCL01_problems_CLB003.c
@@ -0,0 +1,23 @@
+// Printing text
+// In the last problem, we saw how to print a number(using printf()).
+
+// To print a number we write this:
+
+// printf("%d", 42);
+// But to output a text, we do this
+
+// printf("I love C");
+// This will print I love C to the screen. No need to add any %d or anything. Just the text you want to print inside quotes ("").
+
+// Task
+// Replace the __ (underscores) in the editor with the text "I love C" to output I love C.
+
+#include <stdio.h>
+
+int main() {
+
+  // Replace the _ (underscores) with the correct value
+  printf("I love C");
+  return 0;
+
+}
