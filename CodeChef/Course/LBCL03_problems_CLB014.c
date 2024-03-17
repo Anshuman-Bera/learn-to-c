51b597785a0816f491110c1792339a366bacd756
Anshuman-Bera <anshubera2005@gmail.com>
2024-03-17 12:16:03 +0530

    9th c program

---
+++ CodeChef/Course/LBCL03_problems_CLB014.c
@@ -0,0 +1,22 @@
+// Inserting text Between Outputs
+// In the last lesson, we wanted space between numbers so added space between %d like this:
+
+// printf("%d %d", 2, 3);
+// We can add other text as well.
+
+// Example:
+
+// printf("%d and %d", 2, 3);
+
+// // Output:
+// // 2 and 3
+// Task
+// Write a program which prints "7 and 3", using multiple %d in a single printf statement.
+
+#include <stdio.h>
+
+int main() {
+	// Add printf to output "7 and 3"
+	printf("%d and %d",7,3);
+	return 0;
+}
