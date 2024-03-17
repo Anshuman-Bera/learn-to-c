7674783acc1b2fe79c734120f66e8ab5a437c4ef
Anshuman-Bera <anshubera2005@gmail.com>
2024-03-17 12:52:59 +0530

    16th c program

---
+++ CodeChef/Course/LBCL05_problems_CLB025.c
@@ -0,0 +1,25 @@
+// Area of Rectangle
+// Write a program which does the following
+
+// Find out and display the area of a rectangle of sides 45 and 76 respectively.
+// Declare variables length, width and area and assign the relevant values to them
+// Make sure to add some text, so that the person reading the output knows what the output means.
+// Output Format
+// The Area of the given rectangle is 
+// �
+// �
+// �
+// �
+// area
+
+#include <stdio.h>
+
+int main() {
+
+  int length = 45;
+  int width = 76;
+  int Area = length * width;
+  printf("The Area of the given rectangle is %d", Area);
+  return 0;
+
+}
