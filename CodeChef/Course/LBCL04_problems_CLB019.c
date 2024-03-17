81dec3302b1b7f8cee8145f435626112f655519c
Anshuman-Bera <anshubera2005@gmail.com>
2024-03-17 12:32:20 +0530

    12th c program

---
+++ CodeChef/Course/LBCL04_problems_CLB019.c
@@ -0,0 +1,13 @@
+// Task
+// Now that we have some idea of what these lines mean. I want you to write the complete code in the editor. It will help you in remembering all the lines.
+
+// Write the exact code which we discussed in this lesson, to output the sum of 12 and 7. If you miss anything like curly braces, you will get an error, don't worry, just ask about the error in Help tab.
+
+// Write complete code to print the sum of 12 and 7
+#include<stdio.h>
+int main(){
+    int a =12, b = 7,s;
+    s  = (a+b);
+    printf("%d",s);
+    return 0;
+}
