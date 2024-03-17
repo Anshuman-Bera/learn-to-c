5672ef212e62b354ec9efb0681d9604c2d357fdc
Anshuman-Bera <anshubera2005@gmail.com>
2024-03-17 12:20:00 +0530

    10th c program

---
+++ CodeChef/Course/LBCL04_problems_CLB017.c
@@ -0,0 +1,12 @@
+// Multiplication
+// Write a program which prints these on two separate lines.
+
+// Result of multiplication of 8 and 9
+// Result of (100 + 200) * 2
+#include <stdio.h>
+
+int main() {
+	printf("%d\n", 8 * 9);
+	printf("%d", (100 + 200) * 2);
+	return 0;
+}
