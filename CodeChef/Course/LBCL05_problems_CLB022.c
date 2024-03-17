10aa2d0fe4c083e30a9b61c668710869f964cf96
Anshuman-Bera <anshubera2005@gmail.com>
2024-03-17 12:40:05 +0530

    13th c program

---
+++ CodeChef/Course/LBCL05_problems_CLB022.c
@@ -0,0 +1,12 @@
+// Task
+// Create a variable in editor named age and assign the value 25 to it.
+// Print the value of age variable using print statement.
+
+#include <stdio.h>
+
+int main() {
+    // Replace __ with 25
+    int age = 25;
+    printf("%d", age);
+    return 0;
+}
