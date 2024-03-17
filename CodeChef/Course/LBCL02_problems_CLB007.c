2b3bbad5a58f0b29dc68080ec0295b6bfe92870e
Anshuman-Bera <anshubera2005@gmail.com>
2024-03-17 11:54:31 +0530

    5th c program

---
+++ CodeChef/Course/LBCL02_problems_CLB007.c
@@ -0,0 +1,18 @@
+// Multiple Outputs
+// We can add as many printf() statements as we want in our code.
+
+// Task
+// Write a program which does the following
+
+// Add a printf statement and output the sum of 3 + 4
+// Add another printf statement and output the sum of 2 + 1.
+// Use the printf syntax we learned in last problem.
+
+// Note - Notice that in the output they are printed together without any space between them.
+#include <stdio.h>
+
+int main() {
+	printf("%d",3+4);
+	printf("%d",2+1);
+	return 0;
+}
