806b1a9623240f15ee5a1f313ed0caf6d634027d
Anshuman-Bera <anshubera2005@gmail.com>
2024-03-19 01:33:28 +0530

    30th c program

---
+++ CodeChef/Course/DEBUGCBEG_problems_DEBUGCL3.c
@@ -0,0 +1,14 @@
+// Task
+// Submit the code present in IDE as it is.
+// Read the error statement and understand what needs to be fixed.
+// Fix the code so that it correctly outputs: 15
+
+#include <stdio.h>
+
+int main() {
+   int a = 5;
+   int b = 3;
+   
+   scanf("%d %d", &a, &b);
+   printf("%d", a * b);
+}
