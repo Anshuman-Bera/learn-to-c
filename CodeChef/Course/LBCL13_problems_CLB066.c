2df0cb7350ac86b8dd294d531e5626062d6f064a
Anshuman-Bera <anshubera2005@gmail.com>
2024-03-19 01:14:48 +0530

    27th c program

---
+++ CodeChef/Course/LBCL13_problems_CLB066.c
@@ -0,0 +1,20 @@
+// Task
+// Write a program which does the following
+
+// Take input for two integer variables a & b
+// Output "Coding is Fun" to the console if a is greater than b.
+#include <stdio.h>
+
+int main() {
+    int a,b;
+    scanf("%d%d",&a,&b);
+    if(a>b){
+        printf("Coding is fun");
+    }
+    else{
+        
+    }
+     
+	// your code goes here
+    
+}
